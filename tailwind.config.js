01e9aa0324c7966be2f94c9fa72e95f81ed7cbd9
sid2001sing <148205172+sid2001sing@users.noreply.github.com>
2025-11-25 11:18:42 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          850: '#151e2e',
+          950: '#020617',
+        }
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+}
